aa816c4f21ed14e8f8afe1d496136098fddf77de
Eduard-05 <eduard.pavlenko.05@gmail.com>
2024-04-09 22:28:54 +0200

    fixed_issues_1

--- Sequences/indexes_of_max_and_min_in_the_sequence.py
+++ Sequences/indexes_of_max_and_min_in_the_sequence.py
@@ -7,26 +7,17 @@ num_of_inputs = 0
 
 while a != 0:
 
+    if min_num == None or min_num > a:
+        min_num = a
+        inMin = num_of_inputs
+
+    if max_num == None or max_num < a:
+        max_num = a
+        inMax = num_of_inputs
+
     a = int(input())
-
-    if a == 0:
-        break
-
     num_of_inputs += 1
 
-    if min_num == None:
-        min_num = a
-        inMin = num_of_inputs
-    if min_num > a:
-        min_num = a
-        inMin = num_of_inputs
-
-    if max_num == None:
-        max_num = a
-        inMax = num_of_inputs
-    if max_num < a:
-        max_num = a
-        inMax = num_of_inputs
 
 print("min = ", min_num)
 print("index of min = ", inMin)
--- Sequences/multiplication_of_the_sequence_break_0_0_0.py
+++ Sequences/multiplication_of_the_sequence_break_0_0_0.py
@@ -1,8 +1,6 @@
 a = 1
-i = 0
 zero_counter = 0
-mult0 = 1
-mult1 = None
+mult1 = 1
 mult2 = 1
 prev_zero1 = False
 prev_zero2 = False
@@ -11,31 +9,17 @@ while zero_counter != 3:
 
     a = int(input())
 
-    if mult1 == None:
-       mult1 = a
-
-    if a == 0:
-        if prev_zero2 == True:
-            break
-        if prev_zero1 == True:
-            prev_zero2 == True
-            mult2 = mult1 * 0
-            zero_counter += 1
-        if prev_zero1 == False:
-            prev_zero1 = True
-            mult2 = mult1 * 0
-            zero_counter += 1
-
-    if zero_counter == 3:
-        break
-
-    i += 1
+    if zero_counter == 2 and a == 0:
+        zero_counter += 1
+    if zero_counter == 1 and a == 0:
+        mult2 = mult1 * 0
+        zero_counter += 1
+    if a == 0 and zero_counter == 0:
+        zero_counter += 1
+        mult2 = mult1 * 0
 
     if a != 0:
         zero_counter = 0
-        if i == 1:
-            mult1 = mult2 * a
-        else:
-            mult1 = mult1 * mult2 * a
+        mult1 = mult1 * mult2 * a
 
-    print("multiplication of the row = ", mult1)
+print("multiplication of the row = ", mult1)
