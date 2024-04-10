428fc3d1ecea21b92d69e56461377988fcd20d49
Eduard-05 <155777656+Eduard-05@users.noreply.github.com>
2024-04-11 00:13:25 +0200

    Create max_num_in_array.py

---
+++ Arrays/max_num_in_array.py
@@ -0,0 +1,24 @@
+array = []
+i = 0
+array_size = 0
+array_max = None
+
+print("enter array size: ")
+array_size = int(input())
+
+while i < array_size:
+    
+    print("enter the number: ")
+    array.append(int(input()))
+    i += 1
+
+i = 0
+array_max = array[1]
+
+while i < array_size:
+
+    if array[i] > array_max:
+        array_max = array[i]
+    i += 1
+
+print("array max: ", array_max)
